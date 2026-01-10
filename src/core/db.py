8cfc51cb8987223a096c82118581436acfe7275c
Himanshu Kumar <honey6k6@gmail.com>
2026-01-10 23:13:11 +0530

    Update psycopg dependency in requirements.txt and modify database URL format in configs.py; adjust sessionmaker configuration in db.py

--- src/core/db.py
+++ src/core/db.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from src.core.configs import settings
 
 
-engine = create_engine(settings.database_url, pool_pre_ping=True, future=True)
+engine = create_engine(settings.database_url, pool_pre_ping=True)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
